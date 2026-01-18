68ab0f422cc1a6d37f37f76b2871946781bf40e4
Danilo-dps <danilodpsnilo@outlook.com>
2026-01-18 01:33:17 -0300

    feat: usando variáveis de ambiente

--- src/main/java/com/danilodps/kakfaproducer/producer/KafkaProducer.java
+++ src/main/java/com/danilodps/kakfaproducer/producer/KafkaProducer.java
@@ -3,6 +3,7 @@ package com.danilodps.kakfaproducer.producer;
 import com.danilodps.kakfaproducer.record.response.UserResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaProducer {
 
-    private static final String TOPIC_USER_V1 = "topic.user.v1";
+    @Value("${spring.kafka.producer.topic.kafka-topic}")
+    private String producerTopic;
+
     private final KafkaTemplate <String, Object> kafkaTemplate;
 
     public KafkaProducer(@Qualifier("producer")KafkaTemplate <String, Object> kafkaTemplate){
@@ -19,6 +22,6 @@ public class KafkaProducer {
 
     public void send(UserResponse userResponse){
         log.info("Enviando usuário: {}", userResponse.userId());
-        kafkaTemplate.send(TOPIC_USER_V1, userResponse.userId(), userResponse);
+        kafkaTemplate.send(producerTopic, userResponse.userId(), userResponse);
     }
 }
